9081cc1835440e0141fd2b654c5f9f4d964e5d8f
Adryan PC <adryanisaque@gmail.com>
2026-06-04 14:51:42 -0300

    adicionada correcao de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -22,8 +22,22 @@ app.get('/movies', async (_, res) => {
 app.post('/movies', async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } = req.body
 
+    try {
+        // verficar no banco se ja existe um filme com nome que esta sendo criado
 
-    try{
+        //case insitive - se buscar for feita e tive letra maiusculas ou minuscluas porem a mesma palavra vai retornar
+
+        //case sensitive - caso ocrra de ter letras maiuscolas e etc ele nao vai ser retornado
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: { title: { equals: title, mode: 'insensitive' } },
+        })
+
+        if (movieWithSameTitle) {
+            return res
+                .status(409)
+                .send({ message: 'Ja existe um filme com esse titulo' })
+        }
+
         await prisma.movie.create({
             data: {
                 title,
@@ -32,13 +46,12 @@ app.post('/movies', async (req, res) => {
                 oscar_count,
                 release_date: new Date(release_date),
             },
-        });
-    }catch(error){
-        return res.status(500).send({message:"Falha ao cadastrar filme"})
-    };
+        })
+    } catch (error) {
+        return res.status(500).send({ message: 'Falha ao cadastrar filme' })
+    }
     res.status(201).send()
 })
-
 
 app.listen(port, () => {
     console.log(`Servidor em execucao na porta ${port}`)
